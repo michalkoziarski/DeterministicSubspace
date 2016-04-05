ed6c41f90189a5197b9a798f649826ef4df4c4db
Michał Koziarski <michalkoziarski@gmail.com>
2016-04-05 14:08:58 +0200

    added additional base classifiers

--- script.py
+++ script.py
@@ -20,14 +20,15 @@ classifiers = {
 for dataset_name, dataset in datasets.iteritems():
     for clf_name, clf in classifiers.iteritems():
         for k in [5]:
-            print dataset_name
+            print 'Dataset: %s, base classifier: %s' % (dataset_name, clf_name)
+
             X, y = dataset
             n = X.shape[1] / 2
 
             scores = cross_validation.cross_val_score(
                 RandomSubspaceClassifier(clf, k=k, n=n), X, y, cv=5)
-            print scores.mean()
+            print 'Random Subspace: %.3f' % scores.mean()
 
             scores = cross_validation.cross_val_score(
                 RoundRobinSubspaceClassifier(clf, k=k, n=n), X, y, cv=5)
-            print scores.mean()
+            print 'Deterministic Subspace: %.3f' % scores.mean()
