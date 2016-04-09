47c78a88c8a36ad30bbd26591a1ce3617c328687
Michał Koziarski <michalkoziarski@gmail.com>
2016-04-09 14:07:40 +0200

    printing results to latex table

--- latex.py
+++ latex.py
@@ -14,19 +14,48 @@ def print_table(k, b=None, omega=None):
     print ('\\begin{table}\n'
            '\\caption{}\n'
            '\\centering\n'
-           '\\setlength{\\tabcolsep}{6pt}\n'
-           '\\begin{tabular}{c c c c c c c c c c c c c c c c c c c c c c c c c c}\n'
+           '\\setlength{\\tabcolsep}{2pt}\n'
+           '\\resizebox{\\textwidth}{!}{\n'
+           '\\begin{tabular}{cccccccccccccccccccccccccc}\n'
            '\\hline\\noalign{\\smallskip}\n'
-           'Dataset & RandomForest  & \\multicolumn{6}{l}{SVM} & \\multicolumn{6}{l}{kNN} & \\multicolumn{6}{l}{CART} & \\multicolumn{6}{l}{NaiveBayes} \\\\\n'
+           '\\# & RF & \\multicolumn{6}{l}{SVM} & \\multicolumn{6}{l}{kNN} & \\multicolumn{6}{l}{CART} & \\multicolumn{6}{l}{NaiveBayes} \\\\\n'
+           '\\cmidrule(r){3-8} \\cmidrule(r){9-14} \\cmidrule(r){15-20} \\cmidrule(r){21-26}\n'
+           ' & & & \\multicolumn{5}{l}{DS} & & \\multicolumn{5}{l}{DS} & & \\multicolumn{5}{l}{DS} & & \\multicolumn{5}{l}{DS} \\\\\n'
+           '\\cmidrule(r){4-8} \\cmidrule(r){10-14} \\cmidrule(r){16-20} \\cmidrule(r){22-26}'
+           ' & & RS & 0 & 0.25 & 0.5 & 0.75 & 1 & RS & 0 & 0.25 & 0.5 & 0.75 & 1 & RS & 0 & 0.25 & 0.5 & 0.75 & 1 & RS & 0 & 0.25 & 0.5 & 0.75 & 1 \\\\\n'
            '\\noalign{\\smallskip}\n'
            '\\hline\n'
            '\\noalign{\\smallskip}')
 
-    for dataset in sorted(df['dataset'].unique()):
-        pass
+    datasets = sorted(df['dataset'].unique())
+
+    for i in range(len(datasets)):
+        row = '%d & ' % (i + 1)
+        row += '%.2f & ' % (df[(df['dataset'] == datasets[i]) & (df['classifier'] == 'RandomForest')]['accuracy'])
+
+        for classifier in ['SVM', 'kNN', 'DecisionTree', 'NaiveBayes']:
+            rs_score = df[(df['dataset'] == datasets[i]) &
+                          (df['classifier'] == classifier) &
+                          (df['method'] == 'RandomSubspace')]['accuracy'].iloc[0]
+
+            row += '%.2f & ' % rs_score
+
+            for alpha in ['0.0', '0.25', '0.5', '0.75', '1.0']:
+                ds_score = df[(df['dataset'] == datasets[i]) &
+                              (df['classifier'] == classifier) &
+                              (df['method'] == 'DeterministicSubspace') &
+                              (df['alpha'] == alpha)]['accuracy'].iloc[0]
+
+                if round(ds_score, 2) >= round(rs_score, 2):
+                    row += '\cellcolor[gray]{0.8} '
+
+                row += '%.2f & ' % ds_score
+
+        print row[:-2] + ' \\\\'
 
     print ('\\hline\n'
            '\\end{tabular}\n'
+           '}\n'
            '\\end{table}\n')
 
 
