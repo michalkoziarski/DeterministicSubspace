40aaee9cc587afe03953f80d9416615e69579ea9
Michał Koziarski <michalkoziarski@gmail.com>
2016-04-10 13:42:39 +0200

    script for plotting results

--- plots.py
+++ plots.py
@@ -9,63 +9,64 @@ matplotlib.rc('font', family='Liberation Mono')
 
 
 def plot_accuracy_bars(df):
-    accuracy = []
-    accuracy.append(df[df['method'] == 'RandomSubspace']['accuracy'].mean())
+    for clf in ['SVM', 'kNN', 'NaiveBayes', 'DecisionTree']:
+        accuracy = []
+        accuracy.append(df[(df['method'] == 'RandomSubspace') & (df['classifier'] == clf)]['accuracy'].mean())
 
-    for alpha in ['0.0', '0.25', '0.5', '0.75', '1.0']:
-        accuracy.append(df[(df['method'] == 'DeterministicSubspace') & (df['alpha'] == alpha)]['accuracy'].mean())
+        for alpha in ['0.0', '0.25', '0.5', '0.75', '1.0']:
+            accuracy.append(df[(df['method'] == 'DeterministicSubspace') & (df['classifier'] == clf) & (df['alpha'] == alpha)]['accuracy'].mean())
 
-    indices = range(len(accuracy))
-    labels = ['RS',
-              'DS(0.0)',
-              'DS(0.25)',
-              'DS(0.5)',
-              'DS(0.75)',
-              'DS(1.0)']
+        indices = range(len(accuracy))
+        labels = ['RS',
+                  'DS(0.0)',
+                  'DS(0.25)',
+                  'DS(0.5)',
+                  'DS(0.75)',
+                  'DS(1.0)']
 
-    plt.figure()
-    plt.bar(map(lambda x: x + 0.1, indices), accuracy)
-    plt.xticks(map(lambda x: x + 0.5, indices), labels)
-    plt.xlim((0 - 0.15, len(indices) + 0.15))
-    plt.ylim((0.75, 0.79))
-    plt.title('Average classification accuracy over all datasets', y=1.03)
-    plt.ylabel('Classification accuracy', labelpad=10)
-    plt.xlabel('Subspace generation method: random (RS) and deterministic (DS)')
+        plt.figure()
+        plt.bar(map(lambda x: x + 0.1, indices), accuracy)
+        plt.xticks(map(lambda x: x + 0.5, indices), labels)
+        plt.xlim((0 - 0.15, len(indices) + 0.15))
+        plt.ylim((0., 1.))
+        plt.title('Average accuracy over all datasets for %s' % clf, y=1.03)
+        plt.ylabel('Classification accuracy', labelpad=10)
+        plt.xlabel('Subspace generation method: random (RS) and deterministic (DS)')
 
-    plt.savefig('accuracy_bars.png', transparent=True)
+        plt.savefig('accuracy_bars_%s.png' % clf)
 
 
 def plot_accuracy_k(df):
-    k_values = range(5, 55, 5)
-    methods = ['RS',
-              'DS(0.0)',
-              'DS(0.25)',
-              'DS(0.5)',
-              'DS(0.75)',
-              'DS(1.0)']
-    alphas = ['0.0', '0.25', '0.5', '0.75', '1.0']
+    for clf in ['SVM', 'kNN', 'NaiveBayes', 'DecisionTree']:
+        k_values = range(5, 55, 5)
+        methods = ['RS',
+                  'DS(0.0)',
+                  'DS(0.25)',
+                  'DS(0.5)',
+                  'DS(0.75)',
+                  'DS(1.0)']
+        alphas = ['0.0', '0.25', '0.5', '0.75', '1.0']
 
-    accuracy = [[] for _ in methods]
+        accuracy = [[] for _ in methods]
 
-    for k in k_values:
-        accuracy[0].append(df[(df['method'] == 'RandomSubspace') & (df['k'] == k)]['accuracy'].mean())
+        for k in k_values:
+            accuracy[0].append(df[(df['method'] == 'RandomSubspace') & (df['k'] == k) & (df['classifier'] == clf)]['accuracy'].mean())
 
-        for i in range(len(alphas)):
-            alpha = alphas[i]
-            accuracy[i + 1].append(df[(df['method'] == 'DeterministicSubspace') & (df['alpha'] == alpha) & (df['k'] == k)]['accuracy'].mean())
+            for i in range(len(alphas)):
+                alpha = alphas[i]
+                accuracy[i + 1].append(df[(df['method'] == 'DeterministicSubspace') & (df['alpha'] == alpha) & (df['k'] == k) & (df['classifier'] == clf)]['accuracy'].mean())
 
-    plt.figure()
+        plt.figure()
 
-    for acc in accuracy:
-        plt.plot(k_values, acc)
+        for acc in accuracy:
+            plt.plot(k_values, acc)
 
-    plt.ylim((0.74, 0.79))
-    plt.title('Average classification accuracy over all datasets', y=1.03)
-    plt.ylabel('Classification accuracy', labelpad=10)
-    plt.xlabel('k')
-    plt.legend(methods, loc=4)
+        plt.title('Average accuracy over all datasets for %s' % clf, y=1.03)
+        plt.ylabel('Classification accuracy', labelpad=10)
+        plt.xlabel('k')
+        plt.legend(methods, loc=4)
 
-    plt.savefig('accuracy_k.png', transparent=True)
+        plt.savefig('accuracy_k_%s.png' % clf)
 
 
 if __name__ == '__main__':
