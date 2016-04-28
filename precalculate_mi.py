fb54084eaa0da9cd623e5da87662acc6e9d7cbfb
Michał Koziarski <michalkoziarski@gmail.com>
2016-04-28 13:44:01 +0200

    shuffle during CV; save several shuffled versions of mutual information

--- precalculate_mi.py
+++ precalculate_mi.py
@@ -9,23 +9,26 @@ from sklearn.cross_validation import StratifiedKFold
 
 ROOT_PATH = 'mutual_information'
 CV = 2
+N_ITERATIONS = 5
 
 
 def precalculate(X, y, cv=CV):
-    skf = StratifiedKFold(y, cv)
     folds = []
 
-    for train, test in skf:
-        X_train = X[train]
-        mutual_information = [[0 for _ in range(X_train.shape[1])] for _ in range(X_train.shape[1])]
+    for _ in range(N_ITERATIONS):
+        skf = StratifiedKFold(y, cv, shuffle=True)
 
-        for i in range(X_train.shape[1]):
-            for j in range(i, X_train.shape[1]):
-                mi = mutual_information_2d(X_train[:, i], X_train[:, j], normalized=True)
-                mutual_information[i][j] = mi
-                mutual_information[j][i] = mi
+        for train, test in skf:
+            X_train = X[train]
+            mutual_information = [[0 for _ in range(X_train.shape[1])] for _ in range(X_train.shape[1])]
 
-        folds.append((train, test, mutual_information))
+            for i in range(X_train.shape[1]):
+                for j in range(i, X_train.shape[1]):
+                    mi = mutual_information_2d(X_train[:, i], X_train[:, j], normalized=True)
+                    mutual_information[i][j] = mi
+                    mutual_information[j][i] = mi
+
+            folds.append((train, test, mutual_information))
 
     return folds
 
