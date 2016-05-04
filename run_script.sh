feb22b6b162c336b4524d60ffaadbc1b47511b24
Michał Koziarski <mizzqs@gmail.com>
2016-05-04 14:13:06 -0400

    additional arguments

--- run_script.sh
+++ run_script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 ks=(5 10 15 20 25 30 35 40 45 50)
+clfs=("DecisionTreeClassifier()" "KNeighborsClassifier()" "LinearSVC()" "GaussianNB()")
 
-for k in ${ks[@]}; do
-    while read line; do
-        sbatch batch.sh $line $k
-    done < datasets.txt
+for clf in ${clfs[@]}; do
+    for k in ${ks[@]}; do
+        while read line; do
+            sbatch batch.sh $line $k $clf results DeterministicSubspaceClassifier
+        done < datasets.txt
+    done
 done
