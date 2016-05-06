78bc90a544c3521092b4b6f8d4b8a84036f268d2
Michał Koziarski <mizzqs@gmail.com>
2016-05-06 11:54:05 -0400

    slurm script for plots

---
+++ plots.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=16384
+#SBATCH --time=72:00:00
+
+module add plgrid/tools/python/2.7.9
+
+cd $HOME/DeterministicSubspace
+python plots.py $1
