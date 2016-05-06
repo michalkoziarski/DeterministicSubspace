c7e08b3db1c0712f80544fe529d8951528fe7d3c
Michał Koziarski <mizzqs@gmail.com>
2016-05-06 11:52:04 -0400

    repeat calculations in bash script (since only single cv is processed in main script)

--- run_script.sh
+++ run_script.sh
@@ -3,10 +3,12 @@
 ks=(5 10 15 20 25 30 35 40 45 50)
 clfs=("DecisionTreeClassifier()" "KNeighborsClassifier()" "LinearSVC()" "GaussianNB()")
 
-for clf in ${clfs[@]}; do
-    for k in ${ks[@]}; do
-        while read line; do
-            sbatch batch.sh $line $k $clf results DeterministicSubspaceClassifier
-        done < datasets.txt
+for i in 'seq 1 5'; do
+    for clf in ${clfs[@]}; do
+        for k in ${ks[@]}; do
+            while read line; do
+                sbatch batch.sh $line $k $clf proper_results_corr DeterministicSubspaceClassifier
+            done < datasets.txt
+        done
     done
 done
