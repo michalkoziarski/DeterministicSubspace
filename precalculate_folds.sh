497d0f9a2284569e8d9ab94809d027d8bed7239d
Michał Koziarski <michalkoziarski@gmail.com>
2016-07-06 17:31:09 +0200

    bash script for folds precalculation

---
+++ precalculate_folds.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=72:00:00
+
+module add plgrid/tools/python/2.7.9
+
+cd $HOME/DeterministicSubspace
+python precalculate_folds.py
