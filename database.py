0da03cc00bf5de6b7c61d2acad53b7364f656923
Michał Koziarski <michalkoziarski@gmail.com>
2016-08-09 21:31:38 +0200

    database tools for trial reservation

--- database.py
+++ database.py
@@ -37,8 +37,46 @@ def create():
 def insert(dataset, fold, classifier, method, measure, k, n, alpha, score):
     timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
 
+    if not isinstance(score, basestring):
+        score = round(score, 4)
+
     execute('INSERT INTO trials VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")' %
-            (timestamp, dataset, fold, classifier, method, measure, k, n, alpha, round(score, 4)))
+            (timestamp, dataset, fold, classifier, method, measure, k, n, alpha, score))
+
+
+def reserve(dataset, fold, classifier, method, measure, k, n, alpha):
+    conn = sqlite3.connect(os.path.join(RESULTS_DIR, DB_PATH), timeout=3600.)
+    conn.isolation_level = 'EXCLUSIVE'
+    conn.execute('BEGIN EXCLUSIVE')
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM trials WHERE dataset="%s" AND fold="%s" AND classifier="%s" AND method="%s" '
+                   'AND measure="%s" AND k="%s" AND n="%s" AND alpha="%s"' %
+                   (dataset, fold, classifier, method, measure, k, n, alpha))
+
+    rows = cursor.fetchall()
+
+    if len(rows) > 0:
+        result = False
+    else:
+        result = True
+        timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+
+        cursor.execute('INSERT INTO trials VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "-")' %
+                       (timestamp, dataset, fold, classifier, method, measure, k, n, alpha))
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+
+def update(dataset, fold, classifier, method, measure, k, n, alpha, score):
+    score = round(score, 4)
+
+    execute('UPDATE trials SET score="%s" WHERE dataset="%s" AND fold="%s" AND classifier="%s" AND method="%s" '
+            'AND measure="%s" AND k="%s" AND n="%s" AND alpha="%s"' %
+            (score, dataset, fold, classifier, method, measure, k, n, alpha))
 
 
 def export(path='results.csv'):
