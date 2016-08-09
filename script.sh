8861ae65a7a48888822c6fc09813dae5631170d6
Michał Koziarski <michalkoziarski@gmail.com>
2016-08-09 22:11:16 +0200

    running experiment from single bash script

--- script.sh
+++ script.sh
@@ -6,4 +6,24 @@
 module add plgrid/tools/python/2.7.9
 
 cd $HOME/DeterministicSubspace/DeterministicSubspace
-python script.py -dataset $1 -fold $2 -classifier $3 -method $4 -measure $5 -k $6 -alpha $7
+
+classifiers=("CART" "kNN" "SVM" "NaiveBayes" "ParzenKDE" "NNKDE" "GMM")
+measures=("accuracy" "mutual_information" "correlation")
+
+while read dataset; do
+    for fold in $(seq 0 9); do
+        for k in $(seq 5 5 50); do
+            python script.py -dataset $dataset -fold $fold -classifier RandomForest -k $k
+
+            for classifier in ${classifiers[@]}; do
+                python script.py -dataset $dataset -fold $fold -classifier $classifier -method RS -k $k
+
+                for alpha in $(seq 0.0 0.1 1.0); do
+                    for measure in ${measures[@]}; do
+                        python script.py -dataset $dataset -fold $fold -classifier $classifier -method DS -k $k -measure $measure -alpha $alpha
+                    done
+                done
+            done
+        done
+    done
+done < datase
